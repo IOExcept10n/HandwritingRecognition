3071613593a6410969b10ccb733e58af24f0e7ce
Dmitry Progger <dima_yakovlev_05@list.ru>
2024-05-17 23:51:26 +0500

    XamlPages

--- HandwritingRecognition/EditorPage.xaml.cs
+++ HandwritingRecognition/EditorPage.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace HandwritingRecognition
 {
     /// <summary>
-    /// Логика взаимодействия для HelloWindow.xaml
+    /// Логика взаимодействия для EditorPage.xaml
     /// </summary>
-    public partial class HelloWindow : Window
+    public partial class EditorPage : Page
     {
-        public HelloWindow()
+        public EditorPage()
         {
             InitializeComponent();
         }
